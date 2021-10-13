4ad91b34ef12846d373f5e8019fd7eae60cb97f9
Noralf Trønnes <noralf@tronnes.org>
2021-10-13 13:46:44 +0200

    Expand the license text
    Use the license notice suggested by CC and the Free Software Foundation in
    https://wiki.creativecommons.org/wiki/CC0_FAQ
    
    Suggested-by: @cuvoodoo

--- dln2-devices.h
+++ dln2-devices.h
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: CC0-1.0
+/*
+ * Written in 2021 by Noralf Trønnes <noralf@tronnes.org>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and
+ * neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 
 #ifndef _DLN2_DEVICES_H_
 #define _DLN2_DEVICES_H_
 
-//#include <stdbool.h>
-//#include <stdint.h>
-//#include "common/tusb_common.h"
-
 #include "dln2.h"
-
-
-//#define container_of(ptr, type, member) ({ \
-//            const typeof( ((type *)0)->member ) *__mptr = (ptr);
-//            (type *)( (char *)__mptr - offsetof(type,member) );})
-
 
 struct dln2_i2c_device {
     const char *name;
--- dln2-spi.c
+++ dln2-spi.c
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: CC0-1.0
+/*
+ * Written in 2021 by Noralf Trønnes <noralf@tronnes.org>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and
+ * neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 
 #include <stdio.h>
 #include "dln2.h"
