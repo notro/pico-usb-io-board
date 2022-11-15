7992ae0438236cfd4c9cb4c7d5ccc9276d1bf783
Tao J <tao-j@outlook.com>
2022-11-15 06:15:34 -0500

    add 2 tusb cdc interfaces for uart

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -19,7 +19,17 @@ enum string_index {
     MANUFACTURER_IDX,
     PRODUCT_IDX,
     SERIALNUMBER_IDX,
-    DLN2_INTERFACE_IDX,
+    DLN2_INTERFACE_NAME_IDX,
+    CDC_NAME_IDX,
+};
+
+enum interface_index {
+    DLN_IFCE = 0,
+    CDC1_CMD_IFCE,
+    CDC1_DATA_IFCE,
+    CDC2_CMD_IFCE,
+    CDC2_DATA_IFCE,
+    MAX_N_IFCE,
 };
 
 tusb_desc_device_t const device_descriptor = {
@@ -27,7 +37,7 @@ tusb_desc_device_t const device_descriptor = {
     .bDescriptorType    = TUSB_DESC_DEVICE,
     .bcdUSB          	= 0x0110,
 
-    .bDeviceClass    	= TUSB_CLASS_VENDOR_SPECIFIC,
+    .bDeviceClass    	= 0,
     .bDeviceSubClass 	= 0,
     .bDeviceProtocol 	= 0,
     .bMaxPacketSize0 	= 64,
@@ -60,9 +70,11 @@ uint8_t const *tud_descriptor_device_cb(void)
 
 typedef struct TU_ATTR_PACKED {
     tusb_desc_configuration_t config;
-    tusb_desc_interface_t interface;
-    tusb_desc_endpoint_t bulk_out;
-    tusb_desc_endpoint_t bulk_in;
+    tusb_desc_interface_t dln_interface;
+    tusb_desc_endpoint_t dln_bulk_out;
+    tusb_desc_endpoint_t dln_bulk_in;
+    u_int8_t cdc1_ifce_desc[TUD_CDC_DESC_LEN];
+    u_int8_t cdc2_ifce_desc[TUD_CDC_DESC_LEN];
 } usbtest_source_sink_config_descriptor_t;
 
 usbtest_source_sink_config_descriptor_t source_sink_config_descriptor = {
@@ -70,17 +82,17 @@ usbtest_source_sink_config_descriptor_t source_sink_config_descriptor = {
         .bLength = sizeof(tusb_desc_configuration_t),
         .bDescriptorType = TUSB_DESC_CONFIGURATION,
         .wTotalLength = sizeof(usbtest_source_sink_config_descriptor_t),
-        .bNumInterfaces = 1,
+        .bNumInterfaces = MAX_N_IFCE,
         .bConfigurationValue = 1,
-        .iConfiguration = DLN2_INTERFACE_IDX,
+        .iConfiguration = DLN2_INTERFACE_NAME_IDX,
         .bmAttributes = TU_BIT(7) | TUSB_DESC_CONFIG_ATT_SELF_POWERED,
         .bMaxPower = 100 / 2,
     },
 
-    .interface = {
+    .dln_interface = {
         .bLength = sizeof(tusb_desc_interface_t),
         .bDescriptorType = TUSB_DESC_INTERFACE,
-        .bInterfaceNumber = 0,
+        .bInterfaceNumber = DLN_IFCE,
         .bAlternateSetting = 0,
         .bNumEndpoints = 2,
         .bInterfaceClass = TUSB_CLASS_VENDOR_SPECIFIC,
@@ -89,8 +101,20 @@ usbtest_source_sink_config_descriptor_t source_sink_config_descriptor = {
         .iInterface = 0,
     },
 
-    .bulk_out = DLN2_BULK_DESCRIPTOR(0x01),
-    .bulk_in = DLN2_BULK_DESCRIPTOR(0x81),
+    .dln_bulk_out = DLN2_BULK_DESCRIPTOR(USBD_DLN_EP_OUT),
+    .dln_bulk_in = DLN2_BULK_DESCRIPTOR(USBD_DLN_EP_IN),
+
+    .cdc1_ifce_desc = {
+        TUD_CDC_DESCRIPTOR(CDC1_CMD_IFCE, CDC_NAME_IDX,
+            USBD_CDC_0_EP_CMD, USBD_CDC_CMD_MAX_SIZE,
+            USBD_CDC_0_EP_OUT, USBD_CDC_0_EP_IN, USBD_CDC_IN_OUT_MAX_SIZE)
+    },
+
+    .cdc2_ifce_desc = {
+        TUD_CDC_DESCRIPTOR(CDC2_CMD_IFCE, CDC_NAME_IDX,
+            USBD_CDC_1_EP_CMD, USBD_CDC_CMD_MAX_SIZE,
+            USBD_CDC_1_EP_OUT, USBD_CDC_1_EP_IN, USBD_CDC_IN_OUT_MAX_SIZE)
+    },
 };
 
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
@@ -128,8 +152,10 @@ uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
     } else if (index == SERIALNUMBER_IDX) {
         pico_get_unique_board_id_string(serial, sizeof(serial));
         str = serial;
-    } else if (index == DLN2_INTERFACE_IDX) {
+    } else if (index == DLN2_INTERFACE_NAME_IDX) {
         str = "DLN2";
+    } else if (index == CDC_NAME_IDX) {
+        str = "Pico USB CDC";
     } else {
         return NULL;
     }
