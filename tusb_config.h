635170e74533367bfbfae8c88cfc13f91e456658
Tao J <tao-j@outlook.com>
2022-11-15 06:15:34 -0500

    add 2 tusb cdc interfaces for uart

--- tusb_config.h
+++ tusb_config.h
@@ -20,4 +20,23 @@
 //#undef CFG_TUSB_DEBUG
 //#define CFG_TUSB_DEBUG              2
 
+#define CFG_TUD_CDC 2
+
+#define CFG_TUD_CDC_RX_BUFSIZE 1024
+#define CFG_TUD_CDC_TX_BUFSIZE 1024
+#define USBD_CDC_CMD_MAX_SIZE 8
+#define USBD_CDC_IN_OUT_MAX_SIZE 64
+
+#define USBD_CDC_0_EP_CMD 0x81
+#define USBD_CDC_1_EP_CMD 0x83
+
+#define USBD_CDC_0_EP_OUT 0x01
+#define USBD_CDC_1_EP_OUT 0x03
+
+#define USBD_CDC_0_EP_IN 0x82
+#define USBD_CDC_1_EP_IN 0x84
+
+#define USBD_DLN_EP_IN 0x89
+#define USBD_DLN_EP_OUT 0x09
+
 #endif /* _TUSB_CONFIG_H_ */
